8d9cbc3cbcc29c007c3532dc587ecc7f3dcfd2b4
Mauren <stormqueen1990@gmail.com>
2012-09-30 16:36:39 -0300

    Implemented recognizement function

--- plate_recog.py
+++ plate_recog.py
@@ -35,7 +35,7 @@ class PlateRecognizer:
 		return 1.0 / (1.0 + math.exp(-1.0 * sumVal));
 
 	# Trains a randomly constructed neural net
-	def train(self, pattFile, trainOutputFile, numIter, learnRate, expectedError, stopIter):
+	def train(self, pattFile, trainOutputFile, midLayerSize, learnRate, expectedError, stopIter):
 		# Constructs the initial training matrix
 		inMiddle = [ [ random.random() for i in range(midLayerSize) ] for i in range(LayerSizes.IN) ]
 		middleOut = [ [ random.random() for i in range(LayerSizes.OUT) ] for i in range(midLayerSize) ]
@@ -50,7 +50,7 @@ class PlateRecognizer:
 		fPatt = open(pattFile, "r")
 
 		for line in fPatt:
-			it = fPatt.findall(line)
+			it = regexpPatt.findall(line)
 
 			# Does line contain a pair?
 			if len(it) < 2:
@@ -86,6 +86,74 @@ class PlateRecognizer:
 					if error > itError:
 						itError = error
 
+				# Adjusts the weights
 				if itError > expectedError:
 					for idx in range(LayerSizes.OUT):
-						weightAdjust = delta
+						wAdjOut = delta[idx] * outputOut[idx] * learnRate
+						
+						for i in range(len(middleOut[idx])):
+							middleOut[idx][i] = middleOut[idx][i] - wAdjOut
+
+						wAdjMid = delta[idx] * outputMid[idx] * learnRate
+
+						for i in range(len(inMiddle[idx])):
+							inMiddle[idx][i] = inMiddle[idx][i] - wAdjMid
+
+				numberIt = numberIt + 1
+
+		# Write data to a file
+		with open(trainOutputFile, "w") as f:
+			for line in inMiddle:
+				for val in line:
+					f.write("{!s} ".format(val))
+
+				f.write("\n")
+
+			f.write("\n")
+
+			for line in middleOut:
+				for val in line:
+					f.write("{!s} ".format(val))
+
+				f.write("\n")
+
+			f.close()
+
+	# Recognizes a pattern
+	def recognize(self, netFilename, pattern):
+		inMiddle = []
+		middleOut = []
+
+		numberRegexp = re.compile("([0-9]+\.[0-9]+)")
+		idx = 0
+
+		# Read the selected train file
+		with open(netFilename, "r") as f:
+			for line in f:
+				if line.strip():
+					valList = [ float(item) for item in numberRegexp.findall(line) ]
+
+					if idx < 48:
+						inMiddle.append(valList)
+					else:
+						middleOut.append(valList)
+
+				idx = idx + 1
+
+			f.close()
+
+		midLayerSize = len(middleOut)
+
+		# Recognizes the given pattern
+		outputMid = [ None for i in range(midLayerSize) ]
+		outputOut = [ None for i in range(LayerSizes.OUT) ]
+
+		# Compute outputs for middle layer
+		for idx in range(midLayerSize):
+			outputMid[idx] = sum(float(pattern[idx]) * inMiddle[i][idx] for i in range(LayerSizes.IN))
+		
+		# Compute outputs for output layer
+		for idx in range(LayerSizes.OUT):
+			outputOut[idx] = self.__sigFunc(sum(middleOut[i][idx] * outputMid[idx] for i in range(midLayerSize)))
+
+		return outputOut
