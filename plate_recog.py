5945356f92ca7cc8f9d5ae994ca3b11894f19ed1
Mauren <stormqueen1990@gmail.com>
2012-09-30 00:48:30 -0300

    Implemented function for training

--- plate_recog.py
+++ plate_recog.py
@@ -3,6 +3,7 @@
 
 import random
 import re
+import math
 
 # Sizes for the layers
 class NetSizes:
@@ -36,8 +37,12 @@ class PattPair:
 
 # Plate recognizement kernel
 class PlateRecognizer:
+	# Sigmoid function
+	def __sigFunc(self, sumVal):
+		return 1.0 / (1.0 + math.exp(-1.0 * sumVal));
+
 	# Trains a randomly constructed neural net
-	def train(self, pattFile, trainOutputFile, numIter, ):
+	def train(self, pattFile, trainOutputFile, numIter, learnTax, expectedError):
 		# Constructs the initial training matrix
 		inMiddle = [ [ random.random() for i in range(midLayerSize) ] for i in range(IN_LAYER) ]
 		middleOut = [ [ random.random() for i in range(OUT_LAYER) ] for i in range(midLayerSize) ]
@@ -62,6 +67,21 @@ class PlateRecognizer:
 			p = PattPair(it[0], it[1])
 			pattPairs.append(p)
 
-		for pair in pattPairs:
-			for bit in pair.inputPatt:
+		itError = 0
+		convIter = 0
+		while numIter > 0:
+			for pair in pattPairs:
+				output = [ None for i in range(len(pattPairs)) ]
+				error = [ None for i in range(len(pattPairs)) ]
+				idx = 0
 
+				for bit in pair.inputPatt:
+					output[idx] = sum(bit * weight for weight in net[IN_MIDDLE][idx]))
+					idx = idx + 1
+			
+				idx = 0
+				for val in output:
+					output[idx] = self.__sigFunc(sum(val * weight for weight in net[MIDDLE_OUT][idx]))
+					idx = idx + 1
+
+			numIter = numIter - 1
